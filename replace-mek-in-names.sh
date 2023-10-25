4c2794f7e33be7c0094c1ca0e0017c8662608ec0
IsaBison <info@flechs.net>
2023-10-24 22:29:56 -0400

    Mek >>> Mech in line 3's

--- replace-mek-in-names.sh
+++ replace-mek-in-names.sh
@@ -8,7 +8,7 @@ for file in "$DIR"/*; do
     # Ensure that the item being processed is indeed a file
     if [[ -f "$file" ]]; then
         # Use sed to replace "Mek" with "Mech" on the second line of the file
-        sed -i '' '2s/Mek/Mech/g' "$file"
+        sed -i '' '3s/Mek/Mech/g' "$file"
     fi
 done
 
